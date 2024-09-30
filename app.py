2bdca6958a92a84e09dd1b185490897e2e0b25f5
Christian G <flamerattacker7@gmail.com>
2024-09-30 19:48:24 -0400

    Add basic streamlit setup

---
+++ app.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("Multipage Streamlit website")
+st.write("Welcome to Tech Club's official website!")
---
+++ page3.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("Page 3")
+st.write("This is the content of page 1.")
