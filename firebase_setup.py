18580c7adfbae7d078bbf3ea33fe0e8bacbe7273
Christian G <flamerattacker7@gmail.com>
2024-10-28 10:55:57 -0400

    Add auth system

---
+++ firebase_setup.py
@@ -0,0 +1,16 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
+import pyrebase
+from config import config  # Import your Firebase config
+
+# Firebase Setup
+if not firebase_admin._apps:
+    cred = credentials.Certificate('service-account-file.json')
+    firebase_admin.initialize_app(cred)
+
+# Pyrebase Setup
+firebase = pyrebase.initialize_app(config)
+auth_client = firebase.auth()
+
+# Firestore Setup
+db = firestore.client()
